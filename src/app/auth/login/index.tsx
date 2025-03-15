0063a225a3052c8ebd4fd391e55b4bd35c1b4c0d
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2025-03-15 17:47:46 -0300

    improvements: login

--- src/app/auth/login/index.tsx
+++ src/app/auth/login/index.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 import styles from "./styles";
 
 const Login: FC = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Test Login</Text>
+    <View style={styles["container"]}>
+      <Image source={require("@/assets/images/login-bg.png")} style={styles["image"]} />
+
+      <View style={styles["form-container"]}>
+        <View style={styles["form"]}>
+          <Text style={styles["title"]}>Bem-vindo</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- src/app/auth/login/styles.ts
+++ src/app/auth/login/styles.ts
@@ -1,9 +1,30 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {},
-  text: {
+  container: {
+    flex: 1,
+    flexDirection: "column-reverse",
+    position: "relative",
+  },
+  image: {
+    position: "absolute",
+    top: "-8%",
+  },
+  "form-container": {
+    backgroundColor: "white",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    height: "55%",
+    paddingTop: 30,
+    paddingHorizontal: 25,
+  },
+  form: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 28,
     fontFamily: "Montserrat-Bold",
+    color: "#000000",
   },
 });
 
