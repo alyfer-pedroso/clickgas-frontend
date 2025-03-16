b7ac713456c9005f1d117b57af7c9e9da859dc51
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2025-03-16 10:42:09 -0300

    fix: privacy terms text

--- src/data/constants/privacy-terms-text.ts
+++ src/data/constants/privacy-terms-text.ts
@@ -1,63 +1,78 @@
-export const privacyTermsText = `Bem-vindo ao ClickGás, um aplicativo de delivery especializado em fornecimento de gás. Ao acessar e usar os serviços oferecidos pelo aplicativo ClickGás ("Aplicativo"), você concorda com os seguintes Termos de Uso ("Termos"). Caso não concorde com esses Termos, não utilize o Aplicativo.
+export const privacyTermsText = `Bem-vindo ao ClickGás, um aplicativo de delivery especializado em fornecimento de gás. Ao acessar e usar os serviços oferecidos pelo aplicativo ClickGás ("Aplicativo"), você concorda com os seguintes Termos de Privacidade ("Termos"). Caso não concorde com esses Termos, não utilize o Aplicativo.
 
 1. Definições
 
 1.1 Usuário: Qualquer pessoa que acesse ou utilize os serviços do Aplicativo.
-1.2 Serviços: A entrega de gás realizada pelo Aplicativo, incluindo todas as funcionalidades acessadas por meio da plataforma.
-1.3 Fornecedor: Empresas ou prestadores de serviços que fornecem o gás através do Aplicativo.
 
+1.2 Serviços: A entrega de gás realizada pelo Aplicativo, incluindo todas as funcionalidades acessadas por meio da plataforma.
+
+1.3 Fornecedor: Empresas ou prestadores de serviços que fornecem o gás através do Aplicativo.
+\n
 2. Uso do Aplicativo
 
 2.1 O Usuário concorda em utilizar o Aplicativo para a compra de gás de forma legal e responsável, cumprindo as leis locais e regulamentos aplicáveis.
-2.2 O Usuário é responsável por fornecer informações corretas e atualizadas, incluindo dados de entrega, para garantir a realização do serviço.
-2.3 O Aplicativo é destinado ao uso exclusivo de indivíduos maiores de 18 anos. Ao utilizar os Serviços, você declara que possui a idade mínima exigida.
 
+2.2 O Usuário é responsável por fornecer informações corretas e atualizadas, incluindo dados de entrega, para garantir a realização do serviço.
+
+2.3 O Aplicativo é destinado ao uso exclusivo de indivíduos maiores de 18 anos. Ao utilizar os Serviços, você declara que possui a idade mínima exigida.
+\n
 3. Registro e Conta de Usuário
 
 3.1 Para utilizar o Aplicativo, o Usuário pode ser solicitado a criar uma conta, fornecendo informações pessoais, como nome, endereço, telefone e outros dados necessários para a entrega.
-3.2 O Usuário é responsável por manter a confidencialidade de suas credenciais de acesso à conta e concorda em notificar imediatamente o Aplicativo sobre qualquer uso não autorizado de sua conta.
-3.3 O Aplicativo se reserva o direito de suspender ou encerrar contas de Usuários que violarem estes Termos ou agirem de maneira fraudulenta ou prejudicial.
 
+3.2 O Usuário é responsável por manter a confidencialidade de suas credenciais de acesso à conta e concorda em notificar imediatamente o Aplicativo sobre qualquer uso não autorizado de sua conta.
+
+3.3 O Aplicativo se reserva o direito de suspender ou encerrar contas de Usuários que violarem estes Termos ou agirem de maneira fraudulenta ou prejudicial.
+\n
 4. Processamento de Pedidos e Entregas
 
 4.1 O Aplicativo facilita o processamento do pedido de gás, e a entrega será realizada por um Fornecedor parceiro. O tempo estimado de entrega pode variar dependendo da localização e da disponibilidade.
-4.2 O Usuário concorda que a entrega do gás será realizada apenas em endereços previamente cadastrados e não será possível alterar o endereço de entrega após o processamento do pedido.
-4.3 O Aplicativo não se responsabiliza por qualquer falha ou atraso na entrega que seja causada por fatores fora de seu controle, incluindo, mas não se limitando a, condições climáticas adversas, problemas logísticos ou falhas nos serviços dos Fornecedores.
 
+4.2 O Usuário concorda que a entrega do gás será realizada apenas em endereços previamente cadastrados e não será possível alterar o endereço de entrega após o processamento do pedido.
+
+4.3 O Aplicativo não se responsabiliza por qualquer falha ou atraso na entrega que seja causada por fatores fora de seu controle, incluindo, mas não se limitando a, condições climáticas adversas, problemas logísticos ou falhas nos serviços dos Fornecedores.
+\n
 5. Pagamento
 
 5.1 O pagamento pelo gás adquirido será realizado fora do Aplicativo, diretamente com o Fornecedor, seja via transferência bancária, cartão de crédito ou qualquer outro método acordado entre o Usuário e o Fornecedor.
-5.2 O Aplicativo não processa nem armazena as informações de pagamento dos Usuários, limitando-se a facilitar a realização do pedido e a entrega do gás.
-5.3 O Usuário concorda que o Aplicativo não se responsabiliza por qualquer problema relacionado ao pagamento ou à cobrança do gás, uma vez que o processo de pagamento ocorre diretamente entre o Usuário e o Fornecedor.
 
+5.2 O Aplicativo não processa nem armazena as informações de pagamento dos Usuários, limitando-se a facilitar a realização do pedido e a entrega do gás.
+
+5.3 O Usuário concorda que o Aplicativo não se responsabiliza por qualquer problema relacionado ao pagamento ou à cobrança do gás, uma vez que o processo de pagamento ocorre diretamente entre o Usuário e o Fornecedor.
+\n
 6. Cancelamentos e Reembolsos
 
 6.1 O Usuário pode solicitar o cancelamento de um pedido antes que o processo de entrega tenha sido iniciado.
-6.2 Após a confirmação do envio para entrega, não será possível solicitar reembolso ou cancelamento.
-6.3 Qualquer pedido devolvido ou com falha na entrega será avaliado para determinar se o reembolso é aplicável, de acordo com a política de reembolsos do Aplicativo.
 
+6.2 Após a confirmação do envio para entrega, não será possível solicitar reembolso ou cancelamento.
+
+6.3 Qualquer pedido devolvido ou com falha na entrega será avaliado para determinar se o reembolso é aplicável, de acordo com a política de reembolsos do Aplicativo.
+\n
 7. Responsabilidade
 
 7.1 O Aplicativo não será responsável por danos diretos, indiretos ou consequenciais resultantes do uso do Serviço, incluindo, mas não se limitando a, problemas de entrega ou de qualidade do gás fornecido.
-7.2 O Usuário concorda em isentar o Aplicativo de qualquer responsabilidade relacionada ao uso do gás, incluindo danos à propriedade ou lesões causadas por seu manuseio inadequado.
 
+7.2 O Usuário concorda em isentar o Aplicativo de qualquer responsabilidade relacionada ao uso do gás, incluindo danos à propriedade ou lesões causadas por seu manuseio inadequado.
+\n
 8. Propriedade Intelectual
 
 8.1 Todo o conteúdo do Aplicativo, incluindo textos, imagens, logotipos e design, são propriedade exclusiva do Aplicativo ou de seus licenciadores, sendo protegidos por direitos autorais e outras leis de propriedade intelectual.
-8.2 O Usuário concorda em não reproduzir, distribuir, modificar ou criar obras derivadas do conteúdo do Aplicativo sem autorização expressa.
 
+8.2 O Usuário concorda em não reproduzir, distribuir, modificar ou criar obras derivadas do conteúdo do Aplicativo sem autorização expressa.
+\n
 9. Modificação dos Termos
 
 9.1 O Aplicativo pode modificar estes Termos a qualquer momento, com aviso prévio ao Usuário. Quaisquer alterações serão publicadas na plataforma, e o Usuário concorda em revisar periodicamente os Termos atualizados.
-9.2 O uso contínuo do Aplicativo após a modificação dos Termos implica aceitação das alterações.
 
+9.2 O uso contínuo do Aplicativo após a modificação dos Termos implica aceitação das alterações.
+\n
 10. Política de Privacidade
 
 10.1 O uso do Aplicativo está sujeito à nossa Política de Privacidade, que descreve como coletamos, usamos e protegemos as informações do Usuário. Ao usar o Aplicativo, você concorda com os termos da nossa Política de Privacidade.
 
 11. Disposições Gerais
 
-11.1 Estes Termos de Uso são regidos pelas leis de São Paulo - Brasil, e qualquer disputa será resolvida nos tribunais competentes dessa jurisdição.
+11.1 Estes Termos de Privacidade são regidos pelas leis de São Paulo - Brasil, e qualquer disputa será resolvida nos tribunais competentes dessa jurisdição.
 
 11.2 Se qualquer disposição destes Termos for considerada inválida ou inaplicável, as demais disposições permanecerão em vigor.
 
