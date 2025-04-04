909aa003c4eaaabb5d7205f2d02b006f38814efa
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2025-04-03 21:57:52 -0300

    improvements: new subtitle component

--- src/app/auth/client-registration/index.tsx
+++ src/app/auth/client-registration/index.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react";
 import { Image, Text, View } from "react-native";
 
+import { colors } from "@data/constants";
 import { Input, Registration } from "@components/template";
 
 import styles from "./styles";
 import * as utils from "./utils";
-import { colors } from "@/src/data/constants";
 
 const ClientRegistration: FC = () => {
   return (
@@ -26,7 +26,9 @@ const ClientRegistration: FC = () => {
 
       <Text style={styles["subtitle"]}>Onde você quer receber seu pedido?</Text>
 
-      <Text  style={[styles["subtitle"],{color: colors["primary-blue"], width: "100%", marginTop: "auto", opacity: 0.7}]}>O gás poderá enviar comunicações através do seu contato</Text>
+      <Text style={[styles["subtitle"], { color: colors["primary-blue"], width: "100%", marginTop: "auto", opacity: 0.7 }]}>
+        O gás poderá enviar comunicações através do seu contato
+      </Text>
     </Registration>
   );
 };
